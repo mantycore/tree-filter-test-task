fe268a3282266313f97965eb6893782acb29bf2b
Mantycore <mantycore@gmail.com>
2019-03-28 09:01:38 +0300

    Added comment to the task function; removed redundant match flag check

--- app/utils/search.js
+++ app/utils/search.js
@@ -1,8 +1,24 @@
+/*
+   The function takes as a parameter a (root of a) tree and builds its subtree
+   from nodes whose names contain a given subsring (and all their ancestors and descendants).
+
+   Nodes annotated by attributes:
+   * matched attribute is set to true for nodes with matching names, otherwise false
+   * expanded attribute is set to true for nodes with any matching descendants, otherwise false
+
+   ancestorIsMatched is a housekeeping parameter meaning that the current branch was already
+   matched further to the root, so every node from it should be included in the result branch
+   regardless of other factors.
+*/
 export default function search(root, str, ancestorIsMatched = false) {
     let matched = false;
     let expanded = false;
     let children =  [];
 
+    // * The 'Node matches the search' Term means that Node's name contains the 'search'
+    //   [renamed as 'str' to avoid name conflict with the function itself]
+    //   as a substring (case insensitive)
+    // * If Node mathches the search, its mached property must be set to true, otherwise false
     if (root.name.toLowerCase().includes(str)) {
         matched = true;
     }
@@ -10,14 +26,20 @@ export default function search(root, str, ancestorIsMatched = false) {
 
     root.children.forEach(child => {
         let childSubtree = search(child, str, thisOrAncestorIsMatched);
-        if (childSubtree !== null || thisOrAncestorIsMatched) {
+        if (childSubtree !== null) {
             children.push(childSubtree);
+
+            // * If at least one descendant of the Node matches the search, Node's expanded property
+            //   must be set to true, otherwise false
             if (childSubtree.matched || childSubtree.expanded) {
                 expanded = true;
             }
         }
     });
 
+    // * Function doesn't modify original tree
+    // * Node is included in the resulting set if Node, one of its ancestors [== thisOrAnscetorIsMatched],
+    //   or one of its descendants [== expanded] is matched.
     if (expanded || thisOrAncestorIsMatched) {
         return {
             name: root.name,
